0598e71b925ce562a2845633bbbec2d9e688be5f
Hleb Bandarenka <hbanda@ext.uber.com>
2019-05-24 11:42:37 +0300

    fix how determine solution

--- src/Solver.java
+++ src/Solver.java
@@ -1,5 +1,5 @@
 import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
 
 import java.util.Comparator;
 
@@ -8,7 +8,7 @@ public class Solver {
     private final boolean solvable;
 
     private int moves = 0;
-    private Queue<Board> solution = new Queue<>();
+    private Stack<Board> solution = new Stack<>();
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
@@ -28,18 +28,24 @@ public class Solver {
         final MinPQ<BoardWrapper> originMinPQ = new MinPQ<>(
                 Comparator.comparingInt(BoardWrapper::getPriority)
         );
+
         final MinPQ<BoardWrapper> twinMinPQ = new MinPQ<>(
                 Comparator.comparingInt(BoardWrapper::getPriority)
         );
-        originMinPQ.insert(new BoardWrapper(initial, moves));
-        twinMinPQ.insert(new BoardWrapper(initial.twin(), moves));
-        Board originPredecessor = null;
-        Board twinPredecessor = null;
+        originMinPQ.insert(new BoardWrapper(initial, null));
+        twinMinPQ.insert(new BoardWrapper(initial.twin(), null));
         while (true) {
-            final Board originMinBoard = originMinPQ.delMin().getBoard();
-            final Board twinMinBoard = twinMinPQ.delMin().getBoard();
-            solution.enqueue(originMinBoard);
+            BoardWrapper originMinBoardWrapper = originMinPQ.delMin();
+            final Board originMinBoard = originMinBoardWrapper.getBoard();
+            final BoardWrapper twinMinBoardWrapper = twinMinPQ.delMin();
+            final Board twinMinBoard = twinMinBoardWrapper.getBoard();
             if (originMinBoard.isGoal()) {
+                moves = originMinBoardWrapper.getMoves();
+                while (originMinBoardWrapper.getPreviousBoardWrapper() != null) {
+                    solution.push(originMinBoardWrapper.getBoard());
+                    originMinBoardWrapper = originMinBoardWrapper.getPreviousBoardWrapper();
+                }
+                solution.push(originMinBoardWrapper.getBoard());
                 return true;
             }
             if (twinMinBoard.isGoal()) {
@@ -47,31 +53,27 @@ public class Solver {
                 moves = -1;
                 return false;
             }
-            moves++;
-            fillByNeighbors(originMinPQ, originMinBoard, originPredecessor);
-            fillByNeighbors(twinMinPQ, twinMinBoard, twinPredecessor);
-            originPredecessor = originMinBoard;
-            twinPredecessor = twinMinBoard;
+            fillByNeighbors(originMinPQ, originMinBoardWrapper);
+            fillByNeighbors(twinMinPQ, twinMinBoardWrapper);
         }
     }
 
-    private void fillByNeighbors(final MinPQ<BoardWrapper> minPQ, final Board minBoard, final Board predecessor) {
-        final Iterable<Board> neighbors = minBoard.neighbors();
+    private void fillByNeighbors(final MinPQ<BoardWrapper> minPQ, final BoardWrapper minBoardWrapper) {
+        final Iterable<Board> neighbors = minBoardWrapper.getBoard().neighbors();
         for (final Board neighbor : neighbors) {
-            if (neighbor.equals(predecessor)) {
+            if (neighbor.equals(minBoardWrapper.getPreviousBoardWrapper())) {
                 continue;
             }
-            minPQ.insert(new BoardWrapper(neighbor, moves));
+            minPQ.insert(new BoardWrapper(neighbor, minBoardWrapper));
         }
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
-
     public int moves() {
         return moves;
     }
-    // sequence of boards in a shortest solution; null if unsolvable
 
+    // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
         return solution;
     }
@@ -79,10 +81,19 @@ public class Solver {
     private class BoardWrapper {
         private final Board board;
         private final int priority;
+        private final BoardWrapper previousBoardWrapper;
+        private final int moves;
 
-        public BoardWrapper(final Board board, final int moves) {
+        public BoardWrapper(final Board board, final BoardWrapper previousBoardWrapper) {
+            if (previousBoardWrapper == null) {
+                moves = 0;
+            } else {
+                moves = previousBoardWrapper.getMoves() + 1;
+            }
             this.board = board;
+            this.previousBoardWrapper = previousBoardWrapper;
             this.priority = board.manhattan() + moves;
+
         }
 
         public Board getBoard() {
@@ -91,6 +102,14 @@ public class Solver {
 
         public int getPriority() {
             return priority;
+        }
+
+        public BoardWrapper getPreviousBoardWrapper() {
+            return previousBoardWrapper;
+        }
+
+        public int getMoves() {
+            return moves;
         }
     }
 }
